8cfb51485693b902206ab763421c0a09c7bcb640
steffengroenandersen <steffengroenandersen@gmail.com>
2025-02-06 09:16:28 +0100

    Add python modules

---
+++ 03._Modules/python_modules/my_module/__init__.py
@@ -0,0 +1,4 @@
+from my_module.file_one import ClassA
+from my_module.file_two import ClassB
+
+print("Running my_module __init__")
---
+++ 03._Modules/python_modules/my_module/file_one.py
@@ -0,0 +1,6 @@
+class ClassA:
+  def __init__(self):
+    print("Class A initialized")
+
+def internal_function():
+  print("Some internal stuff happens...")
