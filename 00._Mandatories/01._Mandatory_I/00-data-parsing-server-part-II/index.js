db7d856f7c1eb9fe4d3ef7ee1dd29d3fb1f67db2
steffengroenandersen <steffengroenandersen@gmail.com>
2025-02-12 10:08:19 +0100

    Add endpoints

--- 00._Mandatories/01._Mandatory_I/00-data-parsing-server-part-II/index.js
+++ 00._Mandatories/01._Mandatory_I/00-data-parsing-server-part-II/index.js
@@ -2,11 +2,27 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-import data from "./data/data.json" assert { type: "json" };
+import jsonData from "./data/data.json" assert { type: "json" };
 
 app.get("/json", (req, res) => {
   res.header("Content-Type", "application/json");
-  res.send(JSON.stringify(data));
+  res.send(JSON.stringify(jsonData));
+});
+
+app.get("/csv", (req, res) => {
+  res.send("csv");
+});
+
+app.get("/txt", (req, res) => {
+  res.send("txt");
+});
+
+app.get("/xml", (req, res) => {
+  res.send("xml");
+});
+
+app.get("/yaml", (req, res) => {
+  res.send("yaml");
 });
 
 app.listen(PORT, () => {
