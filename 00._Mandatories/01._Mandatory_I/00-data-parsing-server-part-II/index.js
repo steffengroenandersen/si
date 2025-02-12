04d30f3337f752e3850795b02352c9fa107d62a8
steffengroenandersen <steffengroenandersen@gmail.com>
2025-02-12 10:46:15 +0100

    Add parsers and endpoints

--- 00._Mandatories/01._Mandatory_I/00-data-parsing-server-part-II/index.js
+++ 00._Mandatories/01._Mandatory_I/00-data-parsing-server-part-II/index.js
@@ -1,30 +1,82 @@
+import { promises as fs } from "fs";
+import * as xml2js from "xml2js";
+import * as yaml from "yaml";
+import csv from "csvtojson";
+
 import express from "express";
+
 const app = express();
 const PORT = 3000;
 
-import jsonData from "./data/data.json" assert { type: "json" };
+app.get("/json", async (req, res) => {
+  const data = await parseJson("./data/data.json");
+  res.setHeader("Content-Type", "application/json");
+  res.send({ data: data });
+});
 
-app.get("/json", (req, res) => {
+app.get("/csv", async (req, res) => {
+  const data = await parseCsv("./data/data.csv");
   res.header("Content-Type", "application/json");
-  res.send(JSON.stringify(jsonData));
+  res.send({ data: data });
 });
 
-app.get("/csv", (req, res) => {
-  res.send("csv");
+app.get("/txt", async (req, res) => {
+  const data = await parseTxt("./data/data.txt");
+  res.setHeader("Content-Type", "application/json");
+  res.json({ data: data });
 });
 
-app.get("/txt", (req, res) => {
-  res.send("txt");
+app.get("/xml", async (req, res) => {
+  const data = await parseXml("./data/data.xml");
+  res.header("Content-Type", "application/json");
+  res.send({ data: data });
 });
 
-app.get("/xml", (req, res) => {
-  res.send("xml");
-});
-
-app.get("/yaml", (req, res) => {
-  res.send("yaml");
+app.get("/yaml", async (req, res) => {
+  const data = await parseYaml("./data/data.yaml");
+  res.setHeader("Content-Type", "application/json");
+  res.json({ data: data });
 });
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
+
+// UTILS
+
+function parseJson(filePath) {
+  return fs.readFile(filePath, "utf8").then((fileContent) => {
+    return JSON.parse(fileContent);
+  });
+}
+
+function parseCsv(filePath) {
+  return csv().fromFile(filePath);
+}
+
+function parseTxt(filePath) {
+  return fs.readFile(filePath, "utf8").then((fileContent) => {
+    return yaml.parse(fileContent);
+  });
+}
+
+function parseXml(filePath) {
+  return fs.readFile(filePath, "utf8").then((fileContent) => {
+    const parser = new xml2js.Parser({ explicitArray: false, mergeAttrs: true });
+    return new Promise((resolve, reject) => {
+      parser.parseString(fileContent, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  });
+}
+
+function parseYaml(filePath) {
+  return fs.readFile(filePath, "utf8").then((fileContent) => {
+    return yaml.parse(fileContent);
+  });
+}
